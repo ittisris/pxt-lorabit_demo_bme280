40d112aba6a871d9fa175111850e7c64ad9deaed
Itti Srisumalai <i2ktel@hotmail.com>
2019-12-10 03:32:08 +0700

    Updates.

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.AB, function () {
     loraBit.reset()
     loraBit.param_OTAA(
-        "003D23377585E082",
-        "70B3D57ED00219AA",
-        "398AD37EB687A225DC5E3E3A8CACB425"
+    "003D23377585E082",
+    "70B3D57ED00219AA",
+    "398AD37EB687A225DC5E3E3A8CACB425"
     )
     loraBit.join(loraBit_freq_Plan.AS923)
     basic.showString("join lorabit_demo_002")
     basic.clearScreen()
 })
-function SendImmediate() {
+function SendImmediate () {
     interval = input.runningTime()
     conf = true
     images.createImage(`
@@ -54,16 +54,15 @@ conf = false
 let seqno = 0
 loraBit.Verbose(Verbose_Mode.On)
 loraBit.param_Config(
-    2,
-    7,
-    loraBit_ADR.On
+2,
+7,
+loraBit_ADR.On
 )
 basic.forever(function () {
     if (input.runningTime() > interval) {
         if (!(loraBit.available())) {
             basic.showIcon(IconNames.SmallDiamond)
             basic.pause(50)
-            basic.clearScreen()
         } else {
             seqno += 1
             payload = "" + cayenneLPP.lpp_upload() + "6265" + loraBit.toHexString(Math.floor(seqno / 256)) + loraBit.toHexString(seqno % 256)
@@ -72,9 +71,10 @@ basic.forever(function () {
             } else {
                 loraBit.sendPacket(loraBit_Confirmed.Uncomfirmed, 99, payload)
             }
-            interval = input.runningTime() + 30000
+            interval = input.runningTime() + 60000
             loraBit.sleep()
         }
+        basic.clearScreen()
     }
     basic.pause(1000)
 })
